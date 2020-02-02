fcf4f9a7edc36eef2a3f6943b704f187fbc468c0
Akashm29 <akashmishra291997@gmail.com>
2020-02-02 14:49:02 +0530

    update config to use blog's URL

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ module.exports = {
   author: 'Akash',
   githubUrl: 'https://github.com/Akashm29/Blog',
   // replace this by the url where your website will be published
-  siteUrl: 'http://localhost:8000',
+  siteUrl: 'https://app.netlify.com/sites/quizzical-aryabhata-675964/deploys/5e3693f2eda9b11c09a2f0cd',
   social: {
     // leave the social media you do not want to appear as empty strings
     twitter: '',
